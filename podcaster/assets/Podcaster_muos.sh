9892e25efac64db3c55110a23266a290774455fa
Tomas Krupka <6817216+krupkat@users.noreply.github.com>
2025-06-08 12:56:56 +0200

    support muos pixie

--- podcaster/assets/Podcaster_muos.sh
+++ podcaster/assets/Podcaster_muos.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 # HELP: Tiny Podcaster
 # ICON: flip
+# GRID: Tiny Podcaster
 
 . /opt/muos/script/var/func.sh
 
 echo app >/tmp/act_go
 
-PODCASTER_DIR="/mnt/mmc/MUOS/application/.podcaster"
+PODCASTER_DIR="$(GET_VAR "device" "storage/rom/mount")/MUOS/application/Podcaster"
 DB_DIR="/run/muos/storage/save/podcaster"
 
 cd "$PODCASTER_DIR" || exit
