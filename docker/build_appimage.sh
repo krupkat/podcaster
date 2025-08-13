ef86a17eb50240e530b73ce877e4ee24cf97d432
Tomas Krupka <6817216+krupkat@users.noreply.github.com>
2025-08-13 14:38:07 +0200

    appimage in CI

--- docker/build_appimage.sh
+++ docker/build_appimage.sh
@@ -5,11 +5,11 @@ IMAGE_TAG="tiny-podcaster:latest"
 TMP_DIR=$(mktemp -d)
 
 # Build the image and copy the files out
-docker build -t $IMAGE_TAG -f docker/Dockerfile .
+docker build -t $IMAGE_TAG -f docker/Dockerfile "$@" .
 DOCKER_ID=$(docker create $IMAGE_TAG)
 docker cp $DOCKER_ID:/build/. $TMP_DIR
 docker rm $DOCKER_ID
 
 mkdir -p export
-mv $TMP_DIR/podcaster-x86_64.AppImage export/tiny-podcaster-client-$VERSION.AppImage
+mv $TMP_DIR/podcaster-x86_64.AppImage export/tiny-podcaster-$VERSION.AppImage
 rm -rf $TMP_DIR
